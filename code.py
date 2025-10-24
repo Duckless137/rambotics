940ca7a5046a245c6397dc2a08e35e776f2d90ef
Owen Douglass <owenwebs@icloud.com>
2025-10-23 20:10:21 -0500

    Added steering and other various changes.

--- code.py
+++ code.py
@@ -1,20 +1,110 @@
+import board
 import time
-import circuitpython_gizmo
 import pwmio
-import adafruit_motor.servo
+import digitalio
+from adafruit_motor import servo, servo
+from adafruit_simplemath import map_range, constrain 
+from circuitpython_gizmo import Gizmo
 
-gizmo = circuitpython_gizmo.Gizmo()
+# sets the parameters for our motors n stuff (can change as we go)
+pwm_freq = 50  # Hertz
+min_pulse = 1000  # milliseconds
+max_pulse = 2000  # milliseconds
+servo_range = 180  # degrees
 
-motor = adafruit_motor.servo.ContinuousServo(
-    pwmio.PWMOut(gizmo.MOTOR_1, frequency=50),
-    min_pulse=1000,
-    max_pulse=2000
-)
+# the Gizmo object provides access to the data that is held by the field
+# management system and the gizmo system processor
+gizmo = Gizmo()
+
+class Motor(servo.ContinuousServo):
+    def __init__(self, pin):
+        super().__init__(
+            pwmio.PWMOut(pin, frequency=pwm_freq),
+            min_pulse=min_pulse,
+            max_pulse=max_pulse
+        ) 
+
+class Servo(servo.Servo):
+    def __init__(self, pin):
+        super().__init__(
+            pwmio.PWMOut(pin, frequency=pwm_freq),
+            actuation_range=servo_range,
+            min_pulse=min_pulse,
+            max_pulse=max_pulse
+        ) 
+
+spinner_on = False
+a_pressed = False
+motor_spinner = Motor(gizmo.MOTOR_1)
+
+conveyor_on: bool = False
+y_pressed: bool = False
+motor_conveyor = Motor(gizmo.MOTOR_2)
+
+motor_left = Motor(gizmo.MOTOR_3)
+motor_right = Motor(gizmo.MOTOR_4)
+
+the_flipper = Servo(gizmo.SERVO_1) # Yeah I'm calling it a flipper fuck u 
+flipper_angle = 0.0 
+flipper_rate = 0.8
+
+
+def check_spinner():
+    if gizmo.buttons.a:
+        global a_pressed
+        global spinner_on
+        if not a_pressed:
+            spinner_on = not spinner_on
+            print(f"Toggling spinner (now {spinner_on})")
+            a_pressed = True
+    else:
+        a_pressed = False
+
+def check_conveyor():
+    if gizmo.buttons.y:
+        global y_pressed
+        global conveyor_on
+        if not y_pressed:
+            conveyor_on = not conveyor_on
+            print(f"Toggling conveyor (now {conveyor_on})")
+            y_pressed = True
+    else:
+        y_pressed = False
+
+def commit_flipper(): # No verb for flipper :( 
+    global the_flipper
+    global flipper_angle
+    global flipper_rate
+
+    flipper_angle += flipper_rate
+    if flipper_rate > 0.0:
+        if flipper_angle >= 90.0:
+            flipper_angle = 90.0
+            flipper_rate *= -1
+    else:
+        if flipper_angle <= 0.0:
+            flipper_angle = 0.0
+            flipper_rate *= -1
+
+    # Angle must be converted to int, otherwise we get OOB err.
+    # Actual variable is a float for precision
+    the_flipper.angle = int(flipper_angle) 
+
 
 while True:
     gizmo.refresh()
-    if gizmo.buttons.a:
-        motor.throttle = 1.0
-    else:
-        motor.throttle = 0.0
-    time.sleep(0.01)
+    
+    # Motor movement
+    speed = map_range(gizmo.axes.left_y, 0, 255, -1.0, 1.0)
+    turning = map_range(gizmo.axes.left_x, 0, 255, -1.0, 1.0)
+    motor_left.throttle = constrain( - speed + turning, -1.0, 1.0)
+    motor_right.throttle = constrain(speed + turning, -1.0, 1.0)
+
+    check_spinner()
+    motor_spinner.throttle = int(spinner_on)
+    
+    check_conveyor()
+    motor_conveyor.throttle = int(conveyor_on) / 5
+
+    commit_flipper()
+
